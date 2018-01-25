9dececa1cfec93528d3c80d0939525fa0f743618
Andreas Bosch <35560399+AndreasBosch@users.noreply.github.com>
2018-01-25 23:32:40 +0100

    callback

---
+++ callback.js
@@ -0,0 +1,20 @@
+// let data = [1, 2, 3, 4, 5, 6];
+
+// function logIt(data){
+//     console.log(data)
+//     }
+
+// data.forEach(function (data) {
+//    logIt(data)
+// });
+
+
+function nyHTML(text){
+    console.log('<p>' + text + '</p>');
+}
+
+function mainFunc(text){
+    nyHTML(text);
+}
+
+nyHTML("PayTagz");
